f4d56dfddda6aed5c0b29d571a2d60dc9a995e33
Poommitol Chaicherdkiat <poommitol.c-sse2017@tggs.kmutnb.ac.th>
2017-12-15 06:59:41 +0700

    move file according to Odoo appstore

---
+++ payment_promptpayqr/README.md
@@ -0,0 +1,19 @@
+# PromtpayQR payment acquirer module for Odoo 10.0
+This repository contain the source code for payment acquirer module for Odoo 10.0
+This is the part of SOFTWARE ENGINEERING subject's project, TGGS KMUTNB THAILAND.
+The propose of this work is only for education.
+
+#Changelog
+[1.0.4] - 2017-11-23
+-fix minor files issue
+[1.0.3] - 2017-11-17
+-Final working version of promptpay qr code.
+[1.0.2] - 2017-10-27
+-update the file name and fix the wrong spelling.
+[1.0.2] - 2017-10-18
+-skeleton of our code
+
+
+
+
+
---
+++ payment_promptpayqr/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "PromptPayQR Payment Acquirer",
+    'summary': 'PromptPayQR Payment Acquirer: implementation',
+    'description': """Transfer Payment Acquirer using PromptPay QR code format""",
+    'author': "Poommitol Chaicherdkiat, Nayan Chandra Nath",
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['payment', 'website_sale'],
+    'data': [
+        'views/promptpayqr_transfer_template.xml',
+        'views/promptpayqr_view.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
