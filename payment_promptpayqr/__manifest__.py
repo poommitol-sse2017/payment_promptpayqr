e5e48cd32edbf575d718be0e603d3c58f08ec33c
poommitol-sse2017 <32904463+poommitol-sse2017@users.noreply.github.com>
2017-12-15 07:07:14 +0700

    Update __manifest__.py

--- payment_promptpayqr/__manifest__.py
+++ payment_promptpayqr/__manifest__.py
@@ -3,7 +3,8 @@
 {
     'name': "PromptPayQR Payment Acquirer",
     'summary': 'PromptPayQR Payment Acquirer: implementation',
-    'description': """Transfer Payment Acquirer using PromptPay QR code format""",
+    'description': """Transfer Payment Acquirer using PromptPay QR code format""
+    <img src="/payment_promptpayqr/static/src/img/transfer_icon.png"/>",
     'author': "Poommitol Chaicherdkiat, Nayan Chandra Nath",
     'category': 'Accounting',
     'version': '0.1',
